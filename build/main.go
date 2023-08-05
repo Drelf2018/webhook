9aad4fc39a2b17df4dc20802a18c16a988251082
Drelf2018 <drelf2018@outlook.com>
2023-08-05 14:15:11 +0800

    fix: 这下不需要本仓库clone了

--- build/main.go
+++ build/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/Drelf2018/webhook"
+import (
+	"os"
+	"os/exec"
+
+	"github.com/Drelf2018/webhook"
+)
 
 func main() {
+	_, err := os.Stat("nana7mi.link")
+	if err != nil {
+		exec.Command("git", "clone", "-b", "gh-pages", "https://github.com/Drelf2018/nana7mi.link.git").Run()
+	}
 	webhook.Run(&webhook.Config{})
 }
