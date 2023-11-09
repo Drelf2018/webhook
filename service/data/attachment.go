107303f6c790f04cccb4ab1e5e00fcef9fa616dd
Drelf2018 <drelf2018@outlook.com>
2023-11-09 15:04:00 +0800

    fix: 优化附件读存

--- service/data/attachment.go
+++ service/data/attachment.go
@@ -1,7 +1,6 @@
 package data
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -16,40 +15,29 @@ import (
 	"github.com/gabriel-vasile/mimetype"
 )
 
+var https = regexp.MustCompile(`https?:/`)
+
 // 附件
 type Attachment struct {
 	db.Model
 	// 原网址
-	Url string `form:"url" gorm:"unique;not null" cmps:"1"`
+	Url string `json:"url" form:"url" gorm:"unique;not null" cmps:"1"`
+	// 本站链接
+	Link string `json:"link"`
 	// 本地路径
-	Local string
+	Local string `json:"-"`
 	// 同浏览器 MIME type 附件的媒体类型
-	MIME string
+	MIME string `json:"MIME"`
 }
 
-func (a Attachment) MarshalJSON() ([]byte, error) {
-	b := bytes.NewBufferString(`{"url":"`)
-	b.WriteString(a.Url)
-	b.WriteString(`","local":"`)
-	if a.Path() != "" {
-		b.WriteString(folder)
-		b.WriteString(a.Path())
-	}
-	b.WriteString(`","MIME":"`)
-	b.WriteString(a.MIME)
-	b.WriteString(`"}`)
-	return json.RawMessage(b.Bytes()), nil
+func (a *Attachment) Init() {
+	p := https.ReplaceAllString(a.Url, "")
+	a.Link = folder + p
+	a.Local = filepath.Join(public, p)
 }
 
 func (a Attachment) String() string {
-	return fmt.Sprintf("Attachment(%v, %v%v)", a.ID, a.Url, utils.Ternary(a.MIME == "", "", ", "+a.MIME))
-}
-
-func (a *Attachment) Path() string {
-	if a.Local == "" {
-		a.Local = regexp.MustCompile("https?:/").ReplaceAllString(a.Url, "")
-	}
-	return a.Local
+	return fmt.Sprintf("Attachment(%v, %v)", a.ID, a.Url)
 }
 
 func (a *Attachment) Save(_ any) {
@@ -59,15 +47,14 @@ func (a *Attachment) Save(_ any) {
 	Posts.FirstOrCreate(nil, func() { go asyncio.RetryError(-1, 5, a.Download) }, a, "url = ?", a.Url)
 }
 
-func (a *Attachment) Detect(r *request.Result) {
-	a.MIME = mimetype.Detect(r.Content).String()
+func (a *Attachment) Detect(content []byte) {
+	a.MIME = mimetype.Detect(content).String()
 }
 
-func (a *Attachment) Store(r *request.Result) {
-	dir, _ := filepath.Split(a.Path())
-	dir = filepath.Join(public, dir)
+func (a *Attachment) Store(content []byte) {
+	dir, _ := filepath.Split(a.Local)
 	os.MkdirAll(dir, os.ModePerm)
-	r.Write(filepath.Join(public, a.Path()), os.ModePerm)
+	os.WriteFile(a.Local, content, os.ModePerm)
 }
 
 // 下载附件
@@ -75,12 +62,22 @@ func (a *Attachment) Download() error {
 	if a.Url == "" {
 		return nil
 	}
-	result := request.Get(a.Url, request.Referer("https://weibo.com/"))
+	a.Init()
+
+	// request
+	result := request.Get(a.Url, func(job *request.Job) {
+		job.Headers = request.HEADERS
+		if strings.Contains(a.Url, "sinaimg.cn") {
+			job.Headers["Referer"] = "https://weibo.com/"
+		}
+	})
 	if utils.LogErr(result.Error()) {
 		return result.Error()
 	}
+
 	// 前两个操作都挺费时所以都协程了
-	asyncio.ForFunc(result, a.Detect, a.Store)
+	asyncio.ForFunc(result.Content, a.Detect, a.Store)
+
 	// 判断完类型并保存在本地后再存数据库
 	if err := Posts.DB.Updates(a).Error; utils.LogErr(err) {
 		return err
@@ -91,7 +88,7 @@ func (a *Attachment) Download() error {
 func Save(url string) string {
 	a := &Attachment{Url: url}
 	Posts.FirstOrCreate(nil, func() { asyncio.RetryError(-1, 5, a.Download) }, a, "url = ?", url)
-	return folder + a.Path()
+	return a.Link
 }
 
 // 附件合集
@@ -110,7 +107,5 @@ func (as Attachments) Urls() string {
 }
 
 func (as Attachments) String() string {
-	aList := make([]string, 0, len(as))
-	asyncio.ForEach(as, func(a Attachment) { aList = append(aList, a.String()) })
-	return fmt.Sprintf("[%v]", strings.Join(aList, ", "))
+	return fmt.Sprintf("[%v]", strings.Join(asyncio.ForEachV(as, Attachment.String), ", "))
 }
--- service/data/data.go
+++ service/data/data.go
@@ -3,7 +3,6 @@ package data
 import (
 	"io/fs"
 	"path/filepath"
-	"strings"
 
 	"github.com/Drelf2018/asyncio"
 	"github.com/Drelf2018/webhook/configs"
@@ -11,16 +10,21 @@ import (
 )
 
 var (
-	Posts    db.DB
-	public   string
-	folder   string
-	replacer *strings.Replacer
+	Posts  db.DB
+	public string
+	folder string
 )
+
+func initTestPost() {
+	TestPost.Blogger.Face.Init()
+	TestPost.Repost.Blogger.Face.Init()
+	TestPost.Repost.Attachments[0].Init()
+}
 
 func Init(r *configs.Config) {
 	public = r.Path.Full.Public
 	folder = "/" + r.Path.Public
-	replacer = strings.NewReplacer(public, "", "\\", "/")
+	initTestPost()
 	Posts.SetSqlite(r.Path.Full.Posts).AutoMigrate(&Post{})
 }
 
@@ -28,15 +32,17 @@ func CheckFiles() error {
 	files := make([]string, 0)
 	filepath.Walk(public, func(path string, info fs.FileInfo, err error) error {
 		if !info.IsDir() {
-			files = append(files, replacer.Replace(path))
+			files = append(files, path)
 		}
 		return nil
 	})
-	var a Attachments
-	err := Posts.DB.Not("local IN ?", files).Find(&a).Error
+
+	var as Attachments
+	err := Posts.DB.Not("local IN ?", files).Find(&as).Error
 	if err != nil {
 		return err
 	}
-	asyncio.ForEachPtr(a, func(a *Attachment) { asyncio.RetryError(3, 5, a.Download) })
+
+	asyncio.ForEachPtr(as, func(a *Attachment) { asyncio.RetryError(3, 5, a.Download) })
 	return nil
 }
