c5054f2b6951caaf645bea5b4bbc3f0574c4ba7f
12138 <drelf2018@outlook.com>
2024-12-11 16:59:43 +0800

    fix: 现在查询自身信息时会携带任务的筛选条件

--- api/user.go
+++ api/user.go
@@ -135,7 +135,7 @@ func DeleteTaskID(ctx *gin.Context) (any, error) {
 // 获取自身信息
 func Get(ctx *gin.Context) (any, error) {
 	user := &model.User{UID: GetUID(ctx)}
-	err := UserDB().Preload("Tasks").First(user).Error
+	err := UserDB().Preload("Tasks").Preload("Tasks.Filters").First(user).Error
 	if err != nil {
 		return 1, err
 	}
